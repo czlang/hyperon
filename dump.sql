e7723fdac6784202d0760180aa55e09ece642615
Peter Láng <cz.lang@gmail.com>
2011-03-04 14:48:07 +0100

    sql dump added

---
+++ dump.sql
@@ -0,0 +1,49 @@
+-- Adminer 3.2.0 MySQL dump
+
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = 'SYSTEM';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author_id` int(11) NOT NULL,
+  `date` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `url` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `body` text COLLATE utf8_czech_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  FULLTEXT KEY `post_title` (`title`,`body`),
+  FULLTEXT KEY `post_title_2` (`title`,`body`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE `settings` (
+  `name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `value` text COLLATE utf8_czech_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `realname` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `avatar` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `about` text COLLATE utf8_czech_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `twitter_user_id` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `twitter_token` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `twitter_token_secret` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `facebook_user_id` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `facebook_sig` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `facebook_access_token` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `role` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
+-- 2011-03-04 14:46:13
