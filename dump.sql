b84267b4afdf091085040fa9a3aaf3782ae92a4b
Peter Láng <cz.lang@gmail.com>
2011-03-10 23:20:24 +0100

    sql dump update

--- dump.sql
+++ dump.sql
@@ -1,9 +1,19 @@
 -- Adminer 3.2.0 MySQL dump
-
+i
 SET NAMES utf8;
 SET foreign_key_checks = 0;
 SET time_zone = 'SYSTEM';
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `beeps`;
+CREATE TABLE `beeps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author_id` int(11) NOT NULL,
+  `date` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `beep` text COLLATE utf8_czech_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
@@ -67,4 +77,4 @@ CREATE TABLE `users` (
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 
--- 2011-03-10 19:28:23
+-- 2011-03-10 23:19:31
