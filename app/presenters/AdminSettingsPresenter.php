0ae4669e6a771184140e0c9020c6c92b6069db75
Peter Lang <cz.lang@gmail.com>
2011-05-14 12:24:30 +0200

    master and write branches merged

---
+++ app/presenters/AdminSettingsPresenter.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * 
+ *
+ * @copyright  Copyright (c) 2011 
+ * @package    
+ */
+
+
+
+/**
+ * Admin settings presenter.
+ *
+ * @author     
+ * @package    
+ */
+final class AdminSettingsPresenter extends AdminPresenter
+{
+
+
+
+	protected function startup()
+	{
+		parent::startup();
+	}
+	
+
+	
+
+	public function renderDefault()
+	{		
+		$form = $this['editSettingsForm'];
+
+		if (!$form->isSubmitted()) {
+
+			$settings = new Settings();
+			$settings = $settings->findAll()->fetchPairs('name', 'value');
+
+			if (!$settings) {
+				throw new BadRequestException('Record not found');
+			}			
+			$form->setDefaults($settings);
+		}
+
+	}		
+	
+
+
+	
+	/**
+	 * Edit settings form component factory.
+	 * @return mixed
+	 */
+	protected function createComponentEditSettingsForm()
+	{	
+		$form = new NAppForm;
+
+		$form->addText('web_name', 'Blog Name');
+		$form->addText('web_desc', 'Blog Description');
+		$form->addText('web_email', 'Email');
+		$form->addTextarea('meta_description', 'Meta description');
+		$form->addTextarea('meta_keywords', 'Meta keywords');
+
+		$form->addSubmit('cancel', 'Cancel')->setValidationScope(NULL)->onClick[] = array($this, 'CancelClicked');		
+		$form->addSubmit('send', 'Save')->onClick[] = array($this, 'sendEditSettingsClicked');
+
+
+		return $form;
+	}
+	
+	
+
+
+   /**
+	* Edit settings form clicked.
+	* 
+	*/
+    public function sendEditSettingsClicked(NSubmitButton $button)
+    {
+    	if ($button->getForm()->getValues()){
+			$settings = new Settings();
+			$settings->update($button->getForm()->getValues());
+			$this->flashMessage('Settings updated.');
+			$this->redirect('AdminSettings:');
+
+    	}
+    }
+	
+		
+	
+
+
+   /**
+	* Cancel clicked.
+	* 
+	*/
+    public function CancelClicked(NSubmitButton $button)
+    {    	
+		$this->redirect('AdminSettings:');    	
+    }
+    
+		
+}
